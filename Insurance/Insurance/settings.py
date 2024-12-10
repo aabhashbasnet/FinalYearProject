0f6a12863986324b9e58112cc9fdb34a336a4ca7
Aabhash <aabhashbasnet25@gmail.com>
2024-12-10 18:43:36 +0545

    Your commit message here

--- Insurance/Insurance/settings.py
+++ Insurance/Insurance/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-=y()t@!!niabh1jggqc-a_qvnt+5*!#4zetw)6^^4*b1-o73gy'
+SECRET_KEY = 'django-insecure-0w7eb)evg!g!9_i+zj*k757ekvp))_93%y=1=t%s(42q4d_tnn'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -37,6 +37,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'api.apps.ApiConfig',
+    'rest_framework',
+    'corsheaders',
 ]
 
 MIDDLEWARE = [
@@ -47,6 +50,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
 ]
 
 ROOT_URLCONF = 'Insurance.urls'
@@ -121,3 +125,8 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+CORS_ALLOW_ALL_ORIGINS = True  # This allows all origins (for development purposes)
+CORS_ALLOWED_ORIGINS = [
+    "http://localhost:3000",  # React app port
+]
